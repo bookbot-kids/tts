dfac06c7fe299191117c5e92a471f83a330e617f
duc <duc@appiphany.com.au>
2022-05-27 16:57:50 +0700

    Add word IPA into sample

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,9 +1,15 @@
+import 'dart:io';
+
 import 'package:duration/duration.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
 
 import 'package:flutter/services.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:sembast/sembast.dart';
+import 'package:sembast/sembast_io.dart';
 import 'package:tts/tts.dart';
+import 'package:path/path.dart' as p;
 
 void main() {
   runApp(const MyApp());
@@ -19,15 +25,55 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   final _ttsPlugin = Tts();
   bool _isRunning = false;
-  // '53 20 64 70 91 45 64 37'
-  final _textController = TextEditingController()
-    ..text = 'h ə l oʊ   w ɝ r l d'; // hello world
+  // 'hello world'    'h ə l oʊ   w ɝ r l d'     '53 20 64 70 91 45 64 37'
+  static const _defaultText = 'library aback ables';
+  final _textController = TextEditingController()..text = _defaultText;
   var _result = '';
+  late Database _db;
+  late StoreRef _storeRef;
 
   @override
   void initState() {
     super.initState();
-    initPlatformState();
+    init();
+  }
+
+  Future<List<String>> _findIPA(String word) async {
+    final finder = Finder(
+        filter: Filter.or(
+            [Filter.equals('word', word), Filter.equals('plural', word)]));
+    final record = await _storeRef.findFirst(_db, finder: finder);
+    final value = record?.value;
+    if (value == null) {
+      return [];
+    }
+
+    final isPlural = value['plural'] == word;
+
+    final ipas = (isPlural ? value['usipaPlural'] : value['usipa']) ?? '';
+    return ipas.split('.');
+  }
+
+  List<String> ipaCharacters(Set<String> allIPAs, List ipas) {
+    final result = <String>[];
+    for (String ipa in ipas) {
+      var characters = ipa.characters.toList();
+      for (var i = 0; i < characters.length; i += 2) {
+        if (i < characters.length - 1) {
+          final combine = '${characters[i]}${characters[i + 1]}';
+          if (allIPAs.contains(combine)) {
+            result.add(combine);
+          } else {
+            result.add(characters[i]);
+            result.add(characters[i + 1]);
+          }
+        } else {
+          result.add(characters[i]);
+        }
+      }
+    }
+
+    return result;
   }
 
   Future<void> _speak() async {
@@ -38,23 +84,49 @@ class _MyAppState extends State<MyApp> {
 
     try {
       final startTime = DateTime.now().millisecondsSinceEpoch;
-      final ipas = _textController.text
+      final text = _textController.text;
+      final words = text
           .split(' ')
           .map((e) => e.trim())
           .where((element) => element.isNotEmpty)
           .toList();
-      final map = _ttsPlugin.search(ipas);
-      final inputIds = map['inputIds'] as List<int>;
-      final visemes = map['visemes'] as List<String>;
+
+      List<int> inputIds = [];
+      List<String> visemes = [];
+
+      List wordIPAs = []; // ipa for each word
+      List wirdInputIds = []; // input id for each word
+      final allAvailableIPAs = _ttsPlugin.allIPAs['en']!;
+      for (final word in words) {
+        final ipa = await _findIPA(word);
+        final characters = ipaCharacters(allAvailableIPAs, ipa);
+        wordIPAs.add(characters);
+        final map = _ttsPlugin.search(characters);
+        inputIds.addAll(map['inputIds'] as List<int>);
+        visemes.addAll(map['visemes'] as List<String>);
+        wirdInputIds.add(map['inputIds']);
+      }
+
       final output = await _ttsPlugin.speakText(
           'fastspeech2_quant.tflite', 'mbmelgan.tflite', inputIds, visemes,
           speed: 1);
+      setState(() {
+        _result += '''
+IPA:
+${wordIPAs.join('; ')}
+
+InputIds:
+${wirdInputIds.join('; ')}
+
+''';
+      });
 
       final totalTime = DateTime.now().millisecondsSinceEpoch - startTime;
       // ignore: avoid_print
       print('output: $output');
       setState(() {
-        _result = 'Duration: ${output.join(', ')}\nExecute in ${printDuration(
+        _result +=
+            'Duration:\n ${output.join('\n')}\n\nExecute in ${printDuration(
           Duration(milliseconds: totalTime),
           tersity: DurationTersity.millisecond,
           abbreviated: false,
@@ -70,8 +142,22 @@ class _MyAppState extends State<MyApp> {
     });
   }
 
-  Future<void> initPlatformState() async {
+  Future<void> init() async {
     await _ttsPlugin.loadMapping('assets/tts_mapping.csv');
+
+    // copy word db into storage
+    Directory appDocDir = await getApplicationDocumentsDirectory();
+
+    final assetContent = await rootBundle.load('assets/Word.db');
+    final dbFile = File(p.join(appDocDir.path, 'Word.db'));
+    if (!await dbFile.exists()) {
+      final bytes = assetContent.buffer
+          .asUint8List(assetContent.offsetInBytes, assetContent.lengthInBytes);
+      await dbFile.writeAsBytes(bytes);
+    }
+
+    _db = await databaseFactoryIo.openDatabase(dbFile.path);
+    _storeRef = StoreRef.main();
   }
 
   @override
@@ -100,7 +186,10 @@ class _MyAppState extends State<MyApp> {
                 _speak();
               },
             ),
-            Text(_result)
+            Expanded(
+                child: SingleChildScrollView(
+              child: Text(_result),
+            ))
           ],
         )),
       ),
