f06b0b04be0a51684412cd3374bebd2cca4b1b6e
duc <duc@appiphany.com.au>
2022-06-08 22:42:58 +0700

    Share buffer player to host

--- android/src/main/kotlin/com/bookbot/tts/ProcessorStrategy.kt
+++ android/src/main/kotlin/com/bookbot/tts/ProcessorStrategy.kt
@@ -1,7 +1,10 @@
 package com.bookbot.tts
 
+import com.tensorspeech.tensorflowtts.tts.TtsBufferPlayer
+
 interface ProcessorStrategy {
     fun initModel(modelPaths: List<String>, onCompleted: (modelOutputs: List<String>) -> Unit)
+    fun playBuffer(player: TtsBufferPlayer, audio: FloatArray): Boolean?
 }
 
 /// The singleton class use to share strategy across plugin & target app
--- android/src/main/kotlin/com/tensorspeech/tensorflowtts/tts/TtsBufferPlayer.kt
+++ android/src/main/kotlin/com/tensorspeech/tensorflowtts/tts/TtsBufferPlayer.kt
@@ -5,14 +5,16 @@ import android.media.AudioFormat
 import android.media.AudioManager
 import android.media.AudioTrack
 import android.util.Log
+import com.bookbot.tts.ProcessorHolder
 import com.tensorspeech.tensorflowtts.utils.ThreadPoolManager
 import java.util.concurrent.Future
 import kotlin.math.min
 
-class TtsBufferPlayer(sampleRate: Int) {
-    private val threadPool = ThreadPoolManager.instance.getSingleExecutor("audio")
-    private val bufferSize = AudioTrack.getMinBufferSize(sampleRate, CHANNEL, FORMAT)
-    private val mAudioTrack: AudioTrack = AudioTrack(
+@Suppress("MemberVisibilityCanBePrivate", "CanBeParameter")
+class TtsBufferPlayer(val sampleRate: Int) {
+    val threadPool = ThreadPoolManager.instance.getSingleExecutor("audio")
+    val bufferSize = AudioTrack.getMinBufferSize(sampleRate, CHANNEL, FORMAT)
+    val audioTrack: AudioTrack = AudioTrack(
         AudioAttributes.Builder()
             .setUsage(AudioAttributes.USAGE_MEDIA)
             .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
@@ -26,9 +28,9 @@ class TtsBufferPlayer(sampleRate: Int) {
         AudioTrack.MODE_STREAM, AudioManager.AUDIO_SESSION_ID_GENERATE
     )
 
-    @Volatile private var isPlaying = false
-    @Volatile private var isInterrupt = false
-    private var task: Future<*>? = null
+    @Volatile var isPlaying = false
+    @Volatile var isInterrupt = false
+    var task: Future<*>? = null
 
     fun play(inputIds: List<Int>, audio: FloatArray) {
         Log.d(TAG, "start playing: $inputIds, audio ${audio.size}")
@@ -42,23 +44,25 @@ class TtsBufferPlayer(sampleRate: Int) {
 
     private fun submitTask(audio: FloatArray) {
         task = threadPool.submit {
-            isPlaying = true
-            var index = 0
-            mAudioTrack.play()
-            while (index < audio.size && !isInterrupt) {
-                val buffer = min(bufferSize, audio.size - index)
-                mAudioTrack.write(
-                    audio,
-                    index,
-                    buffer,
-                    AudioTrack.WRITE_BLOCKING
-                )
-                index += bufferSize
-                Log.d(TAG, "play $index")
+            if(ProcessorHolder.processorStrategy?.playBuffer(this, audio) != true) {
+                isPlaying = true
+                var index = 0
+                audioTrack.play()
+                while (index < audio.size && !isInterrupt) {
+                    val buffer = min(bufferSize, audio.size - index)
+                    audioTrack.write(
+                        audio,
+                        index,
+                        buffer,
+                        AudioTrack.WRITE_BLOCKING
+                    )
+                    index += bufferSize
+                    Log.d(TAG, "play $index")
+                }
+                isPlaying = false
+                isInterrupt = false
+                Log.d(TAG, "play completed")
             }
-            isPlaying = false
-            isInterrupt = false
-            Log.d(TAG, "play completed")
         }
     }
 
