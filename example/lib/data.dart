78bdcb9a5a4a1ed3b718b6856503f2ca4110b5c0
duc <duc@appiphany.com.au>
2025-01-16 14:35:24 +0700

    Update mapping and add new input id test

---
+++ example/lib/data.dart
@@ -0,0 +1,41 @@
+// 'hello world'    'h ə l oʊ   w ɝ r l d'     '53 20 64 70 91 45 64 37'
+const defaultEnText =
+    'library aback ables hello world may then them single they she go do it love me so much join us';
+const defaultIdText =
+    'Saat kamu mengetuk kamu akan secara otomatis mengubah halaman ketika kamu selesai kecuali kamu';
+const defaultSwText =
+    'ting ajabu chakula vitabu hatafuti panya kutafuta hiki hivyo siwezi ana mkubwa';
+
+final testInputIds = [
+  [55, 3, 27, 69, 36, 19, 3, 40, 45, 33, 3, 32, 33, 63, 50, 24, 4, 3],
+  [
+    37,
+    56,
+    10,
+    3,
+    40,
+    45,
+    33,
+    3,
+    37,
+    62,
+    38,
+    3,
+    45,
+    3,
+    22,
+    69,
+    29,
+    3,
+    32,
+    33,
+    63,
+    50,
+    24,
+    4,
+    3
+  ],
+  [37, 66, 27, 3, 19, 69, 29, 4, 3],
+  [63, 32, 45, 28, 12, 3],
+  [37, 69, 33, 3, 45, 3, 48, 50, 57, 33, 3, 32, 33, 63, 50, 24, 4, 3]
+];
